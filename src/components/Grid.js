0a4d046359700d92379b69d4d713872569167664
unknown <jmbrown864@gmail.com>
2024-02-28 18:57:46 -0800

    show current guess

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,6 +5,11 @@ export default function Grid({ currentGuess, guesses, turn }) {
   return (
     <div>
         {guesses.map((guess, i) => {
+            // show the users current guess
+            if (turn === i) {
+                return <Row key={i} currentGuess={currentGuess}/>
+            }
+            // show past guesses
             return <Row key={i} guess={guess}/>
         })}
     </div>
